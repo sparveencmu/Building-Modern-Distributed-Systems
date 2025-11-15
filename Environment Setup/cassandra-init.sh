4e645592f0e118372f6bb356dadb95d88b64b6de
Shazia Parveen <90871252+sparveencmu@users.noreply.github.com>
2025-11-14 20:11:34 -0500

    new file

---
+++ Environment Setup/cassandra-init.sh
@@ -0,0 +1,9 @@
+CQL="CREATE KEYSPACE IF NOT EXISTS dev with REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;
+USE dev;
+CREATE TABLE IF NOT EXISTS urls (id text, url text, PRIMARY KEY (id)) WITH default_time_to_live = 300;"
+until echo $CQL | cqlsh; do 
+    echo "cqlsh: Cassandra is unavailable to initiate - will retry later"
+    sleep 5
+done &
+
+exec /docker-entrypoint.sh "$@"
